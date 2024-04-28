9659bb1245afb2013f976e4a62557216d3169c42
Shubham Kumar <kumar.shubham.off@gmail.com>
2024-04-28 12:08:10 +0530

    updated main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,13 @@
-let _ = print_endline "hello"
+[@@@warning "-27"]
+
+let get_name req = Dream.html @@ "Good morning, " ^ (Dream.param req "word")
+let default req = Dream.html @@ "Good morning, world!"
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router 
+  [
+    Dream.get "/" default;
+    Dream.get "/:word" @@ get_name
+  ]
